9deab5cc153e0abb752a291e09d6518076ebb65c
Mr-Mudassar <mudassarmuhammad776@gmail.com>
2024-02-01 18:19:53 +0500

    section three done

--- src/components/section3/card3.tsx
+++ src/components/section3/card3.tsx
@@ -1,21 +1,259 @@
 import React from "react";
 import Image from "next/image";
 import { FaHeart } from "react-icons/fa";
+import Ethereum from "../../../public/Frame 625940.png";
 
 const Card3 = () => {
   return (
-    <div className="w-[100%] xl:w-[24%] ">
-      <div className="card3-1  background rounded-xl mt-12  border-Default bg-Default overflow-hidden">
-        <div className="card3-1-1 w-[100%] h-60 2xl:h-80 text-end items-end">
-          <div className="flex justify-between p-4">
-            <div className="bg-Background rounded-full">
-              <p className="text-CustomLight py-2 px-4">NEW</p>
+    <div className="flex flex-wrap flex-col sm:flex-row justify-between">
+      {/* card 1 */}
+      <div className="w-[100%] sm:w-[49%] lg:w-[24%] my-2">
+        <div className="card3-1  background rounded-xl mt-12  border-Default bg-Default overflow-hidden">
+          <div className="card3-1-1 w-full h-60 2xl:h-80 flex flex-col justify-between p-4">
+            <div className="flex justify-between ">
+              <div className="bg-Background rounded-full">
+                <p className="text-CustomLight py-2 px-4">NEW</p>
+              </div>
+              <div className="bg-Background rounded-full">
+                <p className="p-2 text-Default">
+                  <FaHeart size={24} />
+                </p>
+              </div>
             </div>
-            <div className="bg-Background rounded-full">
-              <p className=" p-2 text-Default">
-                <FaHeart size={24} />
-              </p>
+
+            <div className="flex justify-end">
+              <button className="bg-Default rounded-md px-4 py-2 text-md md:text-xl text-CustomFontColor font-semibold">
+                MINT NOW!
+              </button>
             </div>
+          </div>
+        </div>
+        <p className="text-CustomLight text-2xl font-semibold my-4">
+          Champion Forging Collection Gen1
+        </p>
+        <p className="text-CustomLight text-lg">
+          Check for Katana Inu Ecosystem for Renting, Staking, our Marketplace
+          or for purchasing our $Kata Token:
+        </p>
+
+        <div className="flex lg:flex-col xl:flex-row justify-between mt-6">
+          {/* section 1 */}
+          <div className="flex flex-col justify-start text-start items-start ">
+            <p className="text-lg text-CustomLight">Price:</p>
+            <p className="font-[jost] text-Default text-xl font-semibold">
+              0.04 ETH
+            </p>
+          </div>
+
+          <span className="h-10 w-[0.5px] bg-CustomFontColor mt-1 lg:hidden xl:block "></span>
+
+          {/* section 2 */}
+          <div className="flex flex-col justify-start text-start items-start ">
+            <p className="text-lg text-CustomLight">Sold/Total:</p>
+            <p className="font-[jost] text-Default text-xl font-semibold">
+              825/5555
+            </p>
+          </div>
+
+          <span className="h-10 w-[0.5px] bg-CustomFontColor mt-1 lg:hidden xl:block"></span>
+
+          {/* section 3 */}
+          <div className="flex flex-col justify-start text-start items-start ">
+            <div className="text-lg text-CustomLight mx-auto lg:mx-0 xl:mx-auto mb-2 ">
+              <Image src={Ethereum} alt="ethereum" />
+            </div>
+            <p className="font-[jost] text-Default text-xl font-semibold">
+              Ethereum
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* card 2  */}
+      <div className="w-[100%] sm:w-[49%] lg:w-[24%] my-2">
+        <div className="card3-1  background rounded-xl mt-12  border-Default bg-Default overflow-hidden">
+          <div className="card3-1-1 w-full h-60 2xl:h-80 flex flex-col justify-between p-4">
+            <div className="flex justify-between ">
+              <div className="bg-Background rounded-full">
+                <p className="text-CustomLight py-2 px-4">NEW</p>
+              </div>
+              <div className="bg-Background rounded-full">
+                <p className="p-2 text-Default">
+                  <FaHeart size={24} />
+                </p>
+              </div>
+            </div>
+
+            <div className="flex justify-end">
+              <button className="bg-Default rounded-md px-4 py-2 text-md md:text-xl text-CustomFontColor font-semibold">
+                MINT NOW!
+              </button>
+            </div>
+          </div>
+        </div>
+        <p className="text-CustomLight text-2xl font-semibold my-4">
+          Champion Forging Collection Gen1
+        </p>
+        <p className="text-CustomLight text-lg">
+          Check for Katana Inu Ecosystem for Renting, Staking, our Marketplace
+          or for purchasing our $Kata Token:
+        </p>
+
+        <div className="flex lg:flex-col xl:flex-row justify-between mt-6">
+          {/* section 1 */}
+          <div className="flex flex-col justify-start text-start items-start ">
+            <p className="text-lg text-CustomLight">Price:</p>
+            <p className="font-[jost] text-Default text-xl font-semibold">
+              0.04 ETH
+            </p>
+          </div>
+
+          <span className="h-10 w-[0.5px] bg-CustomFontColor mt-1 lg:hidden xl:block "></span>
+
+          {/* section 2 */}
+          <div className="flex flex-col justify-start text-start items-start ">
+            <p className="text-lg text-CustomLight">Sold/Total:</p>
+            <p className="font-[jost] text-Default text-xl font-semibold">
+              825/5555
+            </p>
+          </div>
+
+          <span className="h-10 w-[0.5px] bg-CustomFontColor mt-1 lg:hidden xl:block"></span>
+
+          {/* section 3 */}
+          <div className="flex flex-col justify-start text-start items-start ">
+            <div className="text-lg text-CustomLight mx-auto lg:mx-0 xl:mx-auto mb-2 ">
+              <Image src={Ethereum} alt="ethereum" />
+            </div>
+            <p className="font-[jost] text-Default text-xl font-semibold">
+              Ethereum
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* card 3  */}
+      <div className="w-[100%] sm:w-[49%] lg:w-[24%] my-2">
+        <div className="card3-1  background rounded-xl mt-12  border-Default bg-Default overflow-hidden">
+          <div className="card3-1-1 w-full h-60 2xl:h-80 flex flex-col justify-between p-4">
+            <div className="flex justify-between ">
+              <div className="bg-Background rounded-full">
+                <p className="text-CustomLight py-2 px-4">NEW</p>
+              </div>
+              <div className="bg-Background rounded-full">
+                <p className="p-2 text-Default">
+                  <FaHeart size={24} />
+                </p>
+              </div>
+            </div>
+
+            <div className="flex justify-end">
+              <button className="bg-Default rounded-md px-4 py-2 text-md md:text-xl text-CustomFontColor font-semibold">
+                MINT NOW!
+              </button>
+            </div>
+          </div>
+        </div>
+        <p className="text-CustomLight text-2xl font-semibold my-4">
+          Champion Forging Collection Gen1
+        </p>
+        <p className="text-CustomLight text-lg">
+          Check for Katana Inu Ecosystem for Renting, Staking, our Marketplace
+          or for purchasing our $Kata Token:
+        </p>
+
+        <div className="flex lg:flex-col xl:flex-row justify-between mt-6">
+          {/* section 1 */}
+          <div className="flex flex-col justify-start text-start items-start ">
+            <p className="text-lg text-CustomLight">Price:</p>
+            <p className="font-[jost] text-Default text-xl font-semibold">
+              0.04 ETH
+            </p>
+          </div>
+
+          <span className="h-10 w-[0.5px] bg-CustomFontColor mt-1 lg:hidden xl:block "></span>
+
+          {/* section 2 */}
+          <div className="flex flex-col justify-start text-start items-start ">
+            <p className="text-lg text-CustomLight">Sold/Total:</p>
+            <p className="font-[jost] text-Default text-xl font-semibold">
+              825/5555
+            </p>
+          </div>
+
+          <span className="h-10 w-[0.5px] bg-CustomFontColor mt-1 lg:hidden xl:block"></span>
+
+          {/* section 3 */}
+          <div className="flex flex-col justify-start text-start items-start ">
+            <div className="text-lg text-CustomLight mx-auto lg:mx-0 xl:mx-auto mb-2 ">
+              <Image src={Ethereum} alt="ethereum" />
+            </div>
+            <p className="font-[jost] text-Default text-xl font-semibold">
+              Ethereum
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* card 4  */}
+      <div className="w-[100%] sm:w-[49%] lg:w-[24%] my-2">
+        <div className="card3-1  background rounded-xl mt-12  border-Default bg-Default overflow-hidden">
+          <div className="card3-1-1 w-full h-60 2xl:h-80 flex flex-col justify-between p-4">
+            <div className="flex justify-between ">
+              <div className="bg-Background rounded-full">
+                <p className="text-CustomLight py-2 px-4">NEW</p>
+              </div>
+              <div className="bg-Background rounded-full">
+                <p className="p-2 text-Default">
+                  <FaHeart size={24} />
+                </p>
+              </div>
+            </div>
+
+            <div className="flex justify-end">
+              <button className="bg-Default rounded-md px-4 py-2 text-md md:text-xl text-CustomFontColor font-semibold">
+                MINT NOW!
+              </button>
+            </div>
+          </div>
+        </div>
+        <p className="text-CustomLight text-2xl font-semibold my-4">
+          Champion Forging Collection Gen1
+        </p>
+        <p className="text-CustomLight text-lg">
+          Check for Katana Inu Ecosystem for Renting, Staking, our Marketplace
+          or for purchasing our $Kata Token:
+        </p>
+
+        <div className="flex lg:flex-col xl:flex-row justify-between mt-6">
+          {/* section 1 */}
+          <div className="flex flex-col justify-start text-start items-start ">
+            <p className="text-lg text-CustomLight">Price:</p>
+            <p className="font-[jost] text-Default text-xl font-semibold">
+              0.04 ETH
+            </p>
+          </div>
+
+          <span className="h-10 w-[0.5px] bg-CustomFontColor mt-1 lg:hidden xl:block "></span>
+
+          {/* section 2 */}
+          <div className="flex flex-col justify-start text-start items-start ">
+            <p className="text-lg text-CustomLight">Sold/Total:</p>
+            <p className="font-[jost] text-Default text-xl font-semibold">
+              825/5555
+            </p>
+          </div>
+
+          <span className="h-10 w-[0.5px] bg-CustomFontColor mt-1 lg:hidden xl:block"></span>
+
+          {/* section 3 */}
+          <div className="flex flex-col justify-start text-start items-start ">
+            <div className="text-lg text-CustomLight mx-auto lg:mx-0 xl:mx-auto mb-2 ">
+              <Image src={Ethereum} alt="ethereum" />
+            </div>
+            <p className="font-[jost] text-Default text-xl font-semibold">
+              Ethereum
+            </p>
           </div>
         </div>
       </div>
--- src/components/section3/section-3-main.tsx
+++ src/components/section3/section-3-main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Card3 from "./card3";
-import "./card3.css"
+import "./card3.css";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 
 const Section3Main = () => {
@@ -31,6 +31,32 @@ const Section3Main = () => {
 
       <Card3 />
 
+      <div className="flex flex-col md:flex-row justify-between mt-16 text-center items-center">
+        <div className="flex gap-6">
+          <button className=" text-lg md:text-3xl text-Default underline font-[jost]">
+            UPCOMING IN-GAME NFTS
+          </button>
+          <span className="h-10 w-[0.5px] bg-CustomLight mt-1"></span>
+          <button className=" text-lg md:text-3xl text-CustomLight font-[jost]">
+            UPCOMING IN-GAME NFTS
+          </button>
+          <span className="h-10 w-[0.5px] bg-CustomLight mt-1"></span>
+          <button className=" text-lg md:text-3xl text-CustomLight font-[jost]">
+            UPCOMMING TICKETS
+          </button>
+        </div>
+
+        <div className="flex justify-between gap-2 md:gap-4 text-center items-center ">
+          <button className="border border-[#7A7977]  rounded-md p-4 text-sm md:text-lg text-[#7A7977] font-semibold mt-4">
+            <FaChevronLeft />
+          </button>
+          <button className="bg-Default rounded-md p-4 text-sm md:text-lg text-CustomFontColor font-semibold mt-4">
+            <FaChevronRight />
+          </button>
+        </div>
+      </div>
+      <hr className="mt-6"></hr>
+      <Card3 />
     </div>
   );
 };
